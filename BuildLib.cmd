5a7f0c5d80dc391b5dd34d63f234108eb5ac1b14
arunktha <Arunk.Thakur@basware.com>
2020-04-18 22:27:33 +0530

    build changes

--- BuildLib.cmd
+++ BuildLib.cmd
@@ -1,9 +1,1 @@
-echo Build and restore backend libraries...
-
 call dotnet build .\Backend\BuildBackend.xml
-if not %ERRORLEVEL% == 0 set ERRORS=1
-
-echo Build and restore frontend libraries...
-echo to do..
-
-exit /b %ERRORS%
